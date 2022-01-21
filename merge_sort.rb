5bc25b55bb307b1f1178fe530abcedcfc7cce1b5
Jeroen B <jeroenb04@live.com>
2022-01-21 13:39:46 +0100

    Setup recursive function
    Still need to do the practical merging part.

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,4 +1,15 @@
 def merge_sort(arr)
   return arr if arr.length == 1
 
+  # Sort Left half
+  left = merge_sort(arr[0..(arr.length / 2).floor - 1])
+  
+  # Sort right half
+  right = merge_sort(arr[((arr.length / 2).floor)..-1])
+  # Merge halves
+
 end
+
+arr = [1, 5, 2, 4, 3, 6, 7, 9, 10, 8]
+
+p merge_sort(arr)
