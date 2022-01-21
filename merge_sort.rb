7c8ae437b84ef22457f5cf069bb9f4324b49049d
Jeroen B <jeroenb04@live.com>
2022-01-21 21:09:14 +0100

    Setup merging loop for merge_sort

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,13 +1,23 @@
 def merge_sort(arr)
   return arr if arr.length == 1
 
-  # Sort Left half
   left = merge_sort(arr[0..(arr.length / 2).floor - 1])
-  
-  # Sort right half
   right = merge_sort(arr[((arr.length / 2).floor)..-1])
-  # Merge halves
+  res = []
 
+  loop do
+    mm = [left[0], right[0]].minmax
+    res << mm[0]
+    right.delete(right[0]) if right[0] == mm[0]
+    left.delete(left[0]) if left[0] == mm[0]
+    if right.length.zero? 
+      left.each { |v| res << v }
+      return res
+    elsif left.length.zero?
+      right.each { |v| res << v }
+      return res
+    end
+  end
 end
 
 arr = [1, 5, 2, 4, 3, 6, 7, 9, 10, 8]
